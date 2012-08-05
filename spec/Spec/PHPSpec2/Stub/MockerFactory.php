2eafd9808ffd439db5a91d8e746fcb2664f49864
Marcello Duarte <marcello.duarte@gmail.com>
2012-08-05 17:56:37 +0100

    Describe how mocker factory relates to mocker creater and mocker

--- spec/Spec/PHPSpec2/Stub/MockerFactory.php
+++ spec/Spec/PHPSpec2/Stub/MockerFactory.php
@@ -12,4 +12,15 @@ class MockerFactory implements Specification
         $this->object->createFor('PHPSpec2\Specification')
             ->should_return_an_instance_of('PHPSpec2\Stub\Mocker\MockeryMocker');
     }
+
+    function can_be_created_with_an_alternative_mocker($mockerCreator, $mocker)
+    {
+        $mocker->is_a_mock_of('PHPSpec2\Stub\Mocker\MockerInterface');
+
+        $mockerCreator->is_a_mock_of('PHPSpec2\Stub\Mocker\MockerCreatorInterface');
+        $mockerCreator->createNew('PHPSpec2\Specification')->should_return($mocker);
+
+        $this->object->is_an_instance_of('PHPSpec2\Stub\MockerFactory', array($mockerCreator));
+        $this->object->createFor('PHPSpec2\Specification')->should_be_equal_to($mocker);
+    }
 }
