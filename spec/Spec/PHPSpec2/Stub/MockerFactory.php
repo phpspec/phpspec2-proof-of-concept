12c60e85aa0e6b9819969f3c7ee83e05f3b07d07
everzet <ever.zet@gmail.com>
2012-08-06 14:14:07 +0200

    transformed examples to declarative style

--- spec/Spec/PHPSpec2/Stub/MockerFactory.php
+++ spec/Spec/PHPSpec2/Stub/MockerFactory.php
@@ -8,19 +8,19 @@ class MockerFactory implements Specification
 {
     function creates_a_Mockery_Mocker_by_default()
     {
-        $this->object->is_an_instance_of('PHPSpec2\Stub\MockerFactory');
+        $this->object ->is_an_instance_of('PHPSpec2\Stub\MockerFactory');
+
         $this->object->createFor('PHPSpec2\Specification')
             ->should_return_an_instance_of('PHPSpec2\Stub\Mocker\MockeryMocker');
     }
 
-    function can_be_created_with_an_alternative_mocker($mockerCreator, $mocker)
+    function can_be_created_with_an_alternative_mocker($creator, $mocker)
     {
-        $mocker->is_a_mock_of('PHPSpec2\Stub\Mocker\MockerInterface');
+        $mocker       ->is_a_mock_of('PHPSpec2\Stub\Mocker\MockerInterface');
+        $creator      ->is_a_mock_of('PHPSpec2\Stub\Mocker\MockerCreatorInterface');
+        $this->object ->is_an_instance_of('PHPSpec2\Stub\MockerFactory', array($creator));
 
-        $mockerCreator->is_a_mock_of('PHPSpec2\Stub\Mocker\MockerCreatorInterface');
-        $mockerCreator->createNew('PHPSpec2\Specification')->should_return($mocker);
-
-        $this->object->is_an_instance_of('PHPSpec2\Stub\MockerFactory', array($mockerCreator));
-        $this->object->createFor('PHPSpec2\Specification')->should_be_equal_to($mocker);
+        $creator->createNew('PHPSpec2\Specification')      ->should_return($mocker);
+        $this->object->createFor('PHPSpec2\Specification') ->should_be_equal_to($mocker);
     }
 }
