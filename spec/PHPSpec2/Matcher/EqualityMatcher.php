7e23e4a8993607807c1bbc6b294c6273e9a3a889
Marcello Duarte <marcello.duarte@gmail.com>
2012-08-06 19:21:29 +0100

    Describe the rest of the alias for equality matcher

--- spec/PHPSpec2/Matcher/EqualityMatcher.php
+++ spec/PHPSpec2/Matcher/EqualityMatcher.php
@@ -9,7 +9,39 @@ class EqualityMatcher implements Specification
 {
     private static $NO_ARGUMENTS = array();
 
-    function all_kinds_of_subjects()
+    function described_with($matcher)
+    {
+        $matcher->is_an_instance_of('PHPSpec2\Matcher\EqualityMatcher');
+    }
+
+    function should_support_the_be_alias_matcher_for_all_kinds_of_subjects($matcher)
+    {
+        $this->supports_alias_for_all_kinds('be', $matcher);
+    }
+
+    function should_support_the_be_equal_to_alias_for_all_kinds_of_subjects($matcher)
+    {
+        $this->supports_alias_for_all_kinds('be_equal_to', $matcher);
+    }
+
+    function should_support_return_alias_for_all_kinds_of_subjects($matcher)
+    {
+        $this->supports_alias_for_all_kinds('return', $matcher);
+    }
+
+    function should_support_equal_alias_for_all_kinds_of_subjects($matcher)
+    {
+        $this->supports_alias_for_all_kinds('equal', $matcher);
+    }
+
+    private function supports_alias_for_all_kinds($alias, $matcher)
+    {
+        foreach ($this->all_kinds_of_subjects() as $kind => $subject) {
+            $matcher->supports($alias, $subject, self::$NO_ARGUMENTS)->should_be_true();
+        }
+    }
+
+    private function all_kinds_of_subjects()
     {
         return array(
             'string' => 'some_string',
@@ -19,14 +51,5 @@ class EqualityMatcher implements Specification
             'boolean' => true,
             'resource' => STDIN
         );
-    }
-
-    function should_support_the_be_matcher_for_all_kind_of_subjects()
-    {
-        $this->object->is_an_instance_of('PHPSpec2\Matcher\EqualityMatcher');
-
-        foreach ($this->all_kinds_of_subjects() as $kind => $subject) {
-            $this->object->supports('be', $subject, self::$NO_ARGUMENTS)->should_be_true();
-        }
     }
 }
