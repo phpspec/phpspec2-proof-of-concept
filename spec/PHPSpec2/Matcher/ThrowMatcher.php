bc35667b79602886acaf6798fcf600ca8ad32bd9
everzet <ever.zet@gmail.com>
2012-08-08 19:13:01 +0200

    added return_true alias to TrueMatcher

--- spec/PHPSpec2/Matcher/ThrowMatcher.php
+++ spec/PHPSpec2/Matcher/ThrowMatcher.php
@@ -9,8 +9,9 @@ class ThrowMatcher implements Specification
 {
     function supports_the_throw_alias_for_object_and_exception_name()
     {
-        $this->object->supports('throw', new stdClass, array('\Exception'))
-                ->should_be_true();
+        $this->object
+            ->supports('throw', new stdClass, array('\Exception'))
+            ->should_return_true();
     }
 
     /**
@@ -18,10 +19,12 @@ class ThrowMatcher implements Specification
      */
     function can_specify_a_method_during_which_an_exception_should_be_throw($subject)
     {
-        $subject->someMethod()->will_throw('\Exception');
+        $subject->someMethod()
+            ->will_throw('\Exception');
 
-        $this->object->positiveMatch('throw', $subject, array('\Exception'))
-                ->during('someMethod', array());
+        $this->object
+            ->positiveMatch('throw', $subject, array('\Exception'))
+            ->during('someMethod', array());
     }
 
     /**
@@ -29,7 +32,8 @@ class ThrowMatcher implements Specification
      */
     function can_specify_a_method_during_which_an_exception_should_not_be_throw($subject)
     {
-        $this->object->negativeMatch('throw', $subject, array('\Exception'))
-                ->during('someMethod', array());
+        $this->object
+            ->negativeMatch('throw', $subject, array('\Exception'))
+            ->during('someMethod', array());
     }
 }
