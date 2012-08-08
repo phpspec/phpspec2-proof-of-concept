f6d0e5d4cabf8980535dbc398715878097abd650
everzet <ever.zet@gmail.com>
2012-08-08 19:00:35 +0200

    added simple docComment parser for simple mocks

--- spec/PHPSpec2/Matcher/ThrowMatcher.php
+++ spec/PHPSpec2/Matcher/ThrowMatcher.php
@@ -13,22 +13,23 @@ class ThrowMatcher implements Specification
                 ->should_be_true();
     }
 
+    /**
+     * @param ObjectStub $subject mock of stdClass
+     */
     function can_specify_a_method_during_which_an_exception_should_be_throw($subject)
     {
-        $subject->is_a_mock_of('stdClass');
         $subject->someMethod()->will_throw('\Exception');
 
         $this->object->positiveMatch('throw', $subject, array('\Exception'))
                 ->during('someMethod', array());
     }
 
+    /**
+     * @param ObjectStub $subject mock of stdClass
+     */
     function can_specify_a_method_during_which_an_exception_should_not_be_throw($subject)
     {
-        $subject->is_a_mock_of('stdClass');
-        $subject->someMethod();
-
         $this->object->negativeMatch('throw', $subject, array('\Exception'))
                 ->during('someMethod', array());
     }
-
 }
