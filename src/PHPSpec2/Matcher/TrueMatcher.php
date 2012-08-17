a15cca031dfa91d1b74535154d43a2ee7aad75c7
Marcello Duarte <marcello.duarte@gmail.com>
2012-08-17 19:24:27 +0100

    Describe true matcher and add missing negative matching message

--- src/PHPSpec2/Matcher/TrueMatcher.php
+++ src/PHPSpec2/Matcher/TrueMatcher.php
@@ -7,9 +7,11 @@ use PHPSpec2\Exception\Example\BooleanNotEqualException;
 class TrueMatcher extends BasicMatcher
 {
     private $actual;
+    private $usedAlias;
 
     public function supports($name, $subject, array $arguments)
     {
+        $this->usedAlias = $name;
         return in_array($name, array('be_true', 'return_true'));
     }
 
@@ -21,13 +23,16 @@ class TrueMatcher extends BasicMatcher
 
     protected function getFailureException($name, $subject, array $arguments)
     {
+        $type = $this->actual === false ? 'false' : gettype($this->actual);
         return new BooleanNotEqualException(
-            'Expected true got ' . $this->actual, true, $this->actual
+            'Expected true got ' . $type . 'using (' . $this->usedAlias . ')', true, $this->actual
         );
     }
 
     protected function getNegativeFailureException($name, $subject, array $arguments)
     {
-
+        return new BooleanNotEqualException(
+            'Expected not to be true, got true using (' . $this->usedAlias . ')', true, $this->actual
+        );
     }
 }
