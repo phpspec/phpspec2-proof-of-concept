9190861235c2926246b0b63e752a80b64d6fcc83
everzet <ever.zet@gmail.com>
2012-09-09 14:00:35 +0200

    updated deps and fixed runner

--- src/PHPSpec2/Console/Application.php
+++ src/PHPSpec2/Console/Application.php
@@ -3,6 +3,7 @@
 namespace PHPSpec2\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
@@ -21,17 +22,31 @@ class Application extends BaseApplication
     }
 
     /**
-     * Gets the name of the command based on input.
+     * Gets the default input definition.
      *
-     * @param InputInterface $input The input interface
-     *
-     * @return string The command name
+     * @return InputDefinition An InputDefinition instance
      */
+    protected function getDefaultInputDefinition()
+    {
+        return new InputDefinition(array(
+            new InputArgument('command', InputArgument::OPTIONAL, 'The command to execute'),
+
+            new InputOption('--help',           '-h', InputOption::VALUE_NONE, 'Display this help message.'),
+            new InputOption('--quiet',          '-q', InputOption::VALUE_NONE, 'Do not output any message.'),
+            new InputOption('--verbose',        '-v', InputOption::VALUE_NONE, 'Increase verbosity of messages.'),
+            new InputOption('--version',        '-V', InputOption::VALUE_NONE, 'Display this application version.'),
+            new InputOption('--ansi',           '',   InputOption::VALUE_NONE, 'Force ANSI output.'),
+            new InputOption('--no-ansi',        '',   InputOption::VALUE_NONE, 'Disable ANSI output.'),
+            new InputOption('--no-interaction', '-n', InputOption::VALUE_NONE, 'Do not ask any interactive question.'),
+        ));
+    }
+
     protected function getCommandName(InputInterface $input)
     {
         if (null === $command = $input->getFirstArgument()) {
             $command = 'run';
         }
+
         return $command;
     }
 }
