09fdb2d09efe53a545c1a7cb57361ce60624d39e
everzet <ever.zet@gmail.com>
2012-09-09 13:17:43 +0200

    simplified locator public api

--- src/PHPSpec2/Locator.php
+++ src/PHPSpec2/Locator.php
@@ -9,30 +9,18 @@ use ReflectionClass;
 
 class Locator
 {
-    private $path;
-    private $root;
-
-    public function __construct($specsPath = null, $specsRoot = 'spec')
-    {
-        $specsPath = null !== $specsPath ? realpath($specsPath) : null;
-        $specsRoot = realpath($specsRoot);
-
-        $this->path = rtrim($specsPath ?: $specsRoot, DIRECTORY_SEPARATOR);
-        $this->root = rtrim($specsRoot, DIRECTORY_SEPARATOR);
-    }
-
-    public function getSpecifications()
+    public function getSpecifications($path)
     {
         $specs = array();
-        if (is_dir($this->path)) {
-            $files = Finder::create()->files()->name('*.php')->in($this->path);
+        if (is_dir($path)) {
+            $files = Finder::create()->files()->name('*.php')->in($path);
             foreach ($files as $file) {
                 if ($fromFile = $this->getSpecificationsFromFile($file)) {
                     $specs = array_merge($specs, $fromFile);
                 }
             }
-        } elseif (is_file($this->path)) {
-            $file = new SplFileInfo(realpath($this->path));
+        } elseif (is_file($path)) {
+            $file = new SplFileInfo(realpath($path));
             if ($fromFile = $this->getSpecificationsFromFile($file)) {
                 $specs = array_merge($specs, $fromFile);
             }
