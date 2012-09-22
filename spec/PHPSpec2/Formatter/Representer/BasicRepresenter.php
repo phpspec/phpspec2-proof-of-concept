57b2c6ebab7f48a464fabce16df4577a950335e2
everzet <ever.zet@gmail.com>
2012-09-22 18:24:51 +0200

    Remove MethodBehavior (daaaaaaamn yooooooou _md!)
    Marcello raised correct point, that MethodBehavior brings
    our specs scary close to code structure, which could cause
    users to think about system structure, rather than its behavior.

--- spec/PHPSpec2/Formatter/Representer/BasicRepresenter.php
+++ spec/PHPSpec2/Formatter/Representer/BasicRepresenter.php
@@ -7,46 +7,38 @@ use PHPSpec2\MethodBehavior;
 
 class BasicRepresenter extends ObjectBehavior
 {
-    function it_should_represent_any_value()
-    {
-        $this->representValue(42.30)->shouldReturn('double(42.3)');
-    }
-}
-
-class BasicRepresenter_representValue extends MethodBehavior
-{
     function it_should_represent_short_string_by_showing_it()
     {
-        $this('some_string')->shouldReturn('"some_string"');
+        $this->representValue('some_string')->shouldReturn('"some_string"');
     }
 
     function it_should_represent_long_string_by_showing_its_type()
     {
-        $this('some_string_longer_than_thirty_characters')->shouldReturn('[string]');
+        $this->representValue('some_string_longer_than_thirty_characters')->shouldReturn('[string]');
     }
 
     function it_should_represent_integer_by_showing_it()
     {
-        $this(42)->shouldReturn('integer(42)');
+        $this->representValue(42)->shouldReturn('integer(42)');
     }
 
     function it_should_represent_object_as_classname()
     {
-        $this(new \stdClass)->shouldReturn('object(stdClass)');
+        $this->representValue(new \stdClass)->shouldReturn('object(stdClass)');
     }
 
     function it_should_represent_array_as_elements_count()
     {
-        $this(array(1, 2, 3))->shouldReturn('array(3)');
+        $this->representValue(array(1, 2, 3))->shouldReturn('array(3)');
     }
 
     function it_should_represent_boolean_as_string()
     {
-        $this(true)->shouldReturn('true');
+        $this->representValue(true)->shouldReturn('true');
     }
 
     function it_should_represent_closure_as_type()
     {
-        $this(function(){})->shouldReturn('[closure]');
+        $this->representValue(function(){})->shouldReturn('[closure]');
     }
 }
