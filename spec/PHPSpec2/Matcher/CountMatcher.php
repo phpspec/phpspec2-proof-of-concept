352672ca7c8e6524a77e38375be659bb7b96e9da
everzet <ever.zet@gmail.com>
2012-10-03 00:44:30 +0200

    Add missing examples to CountMatcher spec

--- spec/PHPSpec2/Matcher/CountMatcher.php
+++ spec/PHPSpec2/Matcher/CountMatcher.php
@@ -13,8 +13,8 @@ class CountMatcher extends ObjectBehavior
     function described_with($presenter)
     {
         $presenter->presentValue(ANY_ARGUMENTS)->willReturn('countable');
-        $presenter->presentString(ANY_ARGUMENTS)->willReturn('count1');
-        $presenter->presentString(ANY_ARGUMENTS)->willReturn('count2');
+        $presenter->presentString(ANY_ARGUMENTS)->willReturnArgument();
+        $presenter->presentString(ANY_ARGUMENTS)->willReturnArgument();
 
         $this->initializedWith($presenter);
     }
@@ -37,5 +37,37 @@ class CountMatcher extends ObjectBehavior
         $countable->count()->willReturn(4);
 
         $this->shouldNotThrow()->duringPositiveMatch('haveCount', $countable, array(4));
+    }
+
+    function it_should_not_match_wrong_array_count()
+    {
+        $this->shouldThrow(new FailureException('Expected countable to have 2 items, but got 3.'))
+            ->duringPositiveMatch('haveCount', array(1,2,3), array(2));
+    }
+
+    /**
+     * @param ArrayObject $countable
+     */
+    function it_should_not_match_proper_countable_count($countable)
+    {
+        $countable->count()->willReturn(5);
+
+        $this->shouldThrow(new FailureException('Expected countable to have 4 items, but got 5.'))
+            ->duringPositiveMatch('haveCount', $countable, array(4));
+    }
+
+    function it_should_mismatch_wrong_array_count()
+    {
+        $this->shouldNotThrow()->duringNegativeMatch('haveCount', array(1,2,3), array(2));
+    }
+
+    /**
+     * @param ArrayObject $countable
+     */
+    function it_should_mismatch_wrong_countable_count($countable)
+    {
+        $countable->count()->willReturn(5);
+
+        $this->shouldNotThrow()->duringNegativeMatch('haveCount', $countable, array(4));
     }
 }
