399de1fce54eee998d3a0ca6c4ade95b14fd993d
everzet <ever.zet@gmail.com>
2012-10-07 15:19:07 +0200

    Refactor describe command to be much more reliable and flexible

---
+++ src/PHPSpec2/Resources/templates/spec.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace %namespace%;
+
+use PHPSpec2\ObjectBehavior;
+
+class %class% extends ObjectBehavior
+{
+    function it_should_be_initializable()
+    {
+        $this->object->shouldHaveType('%subject%');
+    }
+}
