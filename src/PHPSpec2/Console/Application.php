96a7ca40799ebff1741eb810144dcaa75189c54a
everzet <ever.zet@gmail.com>
2012-10-10 00:36:18 +0200

    Make run default command if no argument providen (closes #23)

--- src/PHPSpec2/Console/Application.php
+++ src/PHPSpec2/Console/Application.php
@@ -3,6 +3,9 @@
 namespace PHPSpec2\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Application extends BaseApplication
@@ -18,5 +21,17 @@ class Application extends BaseApplication
 
         $this->add(new Command\RunCommand($dispatcher));
         $this->add(new Command\DescribeCommand($dispatcher));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        if (!($name = $this->getCommandName($input))) {
+            $input = new ArrayInput(array('command' => 'run'));
+        }
+
+        parent::doRun($input, $output);
     }
 }
