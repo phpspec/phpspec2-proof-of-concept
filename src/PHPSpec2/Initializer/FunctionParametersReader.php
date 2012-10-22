473169df3d93d57b5c9907de83fbed9461140c07
everzet <ever.zet@gmail.com>
2012-10-22 21:41:56 +0200

    Implemented FunctionParametersReader

--- src/PHPSpec2/Initializer/FunctionParametersReader.php
+++ src/PHPSpec2/Initializer/FunctionParametersReader.php
@@ -2,6 +2,64 @@
 
 namespace PHPSpec2\Initializer;
 
+use PHPSpec2\Loader\Node\Example;
+use ReflectionFunction;
+
 class FunctionParametersReader
 {
+    public function getParameters(Example $example)
+    {
+        $parameters = array();
+        foreach ($example->getPreFunctions() as $function) {
+            foreach ($this->getFunctionParameters($function) as $name => $type) {
+                if (!isset($parameters[$name])) {
+                    $parameters[$name] = $type;
+                }
+            }
+        }
+
+        if ($function = $example->getFunction()) {
+            foreach ($this->getFunctionParameters($function) as $name => $type) {
+                if (!isset($parameters[$name])) {
+                    $parameters[$name] = $type;
+                }
+            }
+        }
+
+        foreach ($example->getPostFunctions() as $function) {
+            foreach ($this->getFunctionParameters($function) as $name => $type) {
+                if (!isset($parameters[$name])) {
+                    $parameters[$name] = $type;
+                }
+            }
+        }
+
+        return $parameters;
+    }
+
+    private function getFunctionParameters(ReflectionFunction $function)
+    {
+        $parameters = $this->getDocParameters($function);
+        foreach ($function->getParameters() as $parameter) {
+            if (!isset($parameters[$parameter->getName()])) {
+                $parameters[$parameter->getName()] = null;
+            }
+        }
+
+        return $parameters;
+    }
+
+    private function getDocParameters(ReflectionFunction $function)
+    {
+        $parameters = array();
+        if ($comment = $function->getDocComment()) {
+            foreach (explode("\n", trim($comment)) as $line) {
+                if (preg_match('#@param *([^ ]*) *\$([^ ]*)#', $line, $match)) {
+                    $parameters[$match[2]] = $match[1];
+                }
+            }
+        }
+
+        return $parameters;
+    }
 }
