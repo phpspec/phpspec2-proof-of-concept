ba5e03713e8a4015e8cc85fd23c2ec2c1e6386ab
everzet <ever.zet@gmail.com>
2012-11-01 22:30:21 +0100

    Remove config from ExtensionInterface as DIC holds configuration

--- src/PHPSpec2/Extension/ExtensionInterface.php
+++ src/PHPSpec2/Extension/ExtensionInterface.php
@@ -6,5 +6,5 @@ use PHPSpec2\ServiceContainer;
 
 interface ExtensionInterface
 {
-    public function initialize(ServiceContainer $container, array $config);
+    public function initialize(ServiceContainer $container);
 }
