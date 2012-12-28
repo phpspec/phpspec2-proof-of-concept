0c597c9677037d3c1cdf13d83a61f8ce65ebb0ff
liuggio <liuggio@gmail.com>
2012-12-28 16:05:10 +0100

    First draft of ArrayEngine::Comparison algorithm cleaned a little bit the ArrayEngine::comparison algorithm

--- spec/PHPSpec2/Formatter/Presenter/Differ/ArrayEngine.php
+++ spec/PHPSpec2/Formatter/Presenter/Differ/ArrayEngine.php
@@ -11,17 +11,105 @@ class ArrayEngine extends ObjectBehavior
         $this->supports(array('first'), array('second'))->shouldReturn(true);
     }
 
-    function it_should_calculate_diff()
+    function it_should_calculate_diff_without_recursion()
     {
         $this->compare(array('first'), array('second'))->shouldReturn(<<<STRING
 <code>
-@@ -1,1 +1,1 @@
-<diff-del>-first</diff-del>
-<diff-add>+second</diff-add>
+0 is "first", but expected to be "second"
 </code>
 STRING
         );
 
     }
 
+    function it_should_calculate_diff_for_associative_array()
+    {
+        $this->compare(array('a' => 'first'), array('a' => 'second'))->shouldReturn(<<<STRING
+<code>
+a is "first", but expected to be "second"
+</code>
+STRING
+        );
+    }
+
+
+    function it_should_calculate_diff_for_nested_array()
+    {
+
+    	$arrayExpected = array(
+    		'a1' => array(
+				'a2' => array(
+					'a3' => array(
+						'a4a' => '100', 
+						'a4b' => '102'
+					)
+				)
+			)
+    	);
+
+    	$arrayActual = array(
+    		'a1' => array(
+				'a2' => array(
+					'a3' => array(
+						'a4a' => '100', 
+						'a4b' => '104'
+					)
+				)
+			)
+    	);
+
+        $this->compare($arrayExpected, $arrayActual)->shouldReturn(<<<STRING
+<code>
+a1->a2->a3 a4b is "102", but expected to be "104"
+</code>
+STRING
+        );
+    }
+
+
+    function it_should_calculate_diff_for_nested_array_with_multiple_differences()
+    {
+
+    	$arrayExpected = array(
+    		'a1' => array(
+				'a2' => array(
+					'a3' => array(
+						'a4a' => '100', 
+						'a4b' => '102'
+					)
+				)
+			),
+			'b',
+			'c' => array(
+				'c2'
+			),
+			array(array(array('d')))
+    	);
+
+    	$arrayActual = array(
+    		'a1' => array(
+				'a2' => array(
+					'a3' => array(
+						'a4a' => '100', 
+						'a4b' => '102'
+					)
+				)
+			),
+			'd',
+			'c1' => array(
+				'cd'
+			),
+			array(array(array('s')))
+    	);
+
+        $this->compare($arrayExpected, $arrayActual)->shouldReturn(<<<STRING
+<code>
+0 is "b", but expected to be "d"
+c1 Array does not exists, but expected to be
+1->0->0 0 is "d", but expected to be "s"
+c Array exists, but expected not to be
+</code>
+STRING
+        );
+    }
 }
--- src/PHPSpec2/Formatter/Presenter/Differ/ArrayEngine.php
+++ src/PHPSpec2/Formatter/Presenter/Differ/ArrayEngine.php
@@ -4,6 +4,11 @@ namespace PHPSpec2\Formatter\Presenter\Differ;
 
 class ArrayEngine implements EngineInterface
 {
+    CONST DEFAULT_SEPARATOR = '->';
+    CONST VALUE_NOT_EXPECTED_TO_BE = 1;
+    CONST VALUE_EXPECTED_TO_BE = 2;
+    CONST VALUE_EXPECTED_TO_BE_DIFFERENT = 3;
+
     public function supports($expected, $actual)
     {
         return is_array($expected) && is_array($actual);
@@ -11,22 +16,71 @@ class ArrayEngine implements EngineInterface
 
     public function compare($expected, $actual)
     {
-        $diff = new \Diff($expected, $actual, array());
+        $differences = $this->doComparison($expected, $actual, self::VALUE_NOT_EXPECTED_TO_BE);
+        $differences = array_merge($this->doComparison($actual, $expected, self::VALUE_EXPECTED_TO_BE), $differences);
 
-        $renderer = new \Diff_Renderer_Text_Unified;
-        $text = $diff->render($renderer);
-
-        $lines = array();
-        foreach (explode("\n", $text) as $line) {
-            if (0 === strpos($line, '-')) {
-                $lines[] = sprintf('<diff-del>%s</diff-del>', $line);
-            } elseif (0 === strpos($line, '+')) {
-                $lines[] = sprintf('<diff-add>%s</diff-add>', $line);
-            } else {
-                $lines[] = $line;
-            }
+        $output = '';
+        foreach ($differences as $key => $value) {
+            $output .= sprintf("%s %s\n", trim($key), $value); 
         }
 
-        return sprintf("<code>\n%s</code>", implode("\n", $lines));
+        return sprintf("<code>\n%s</code>", $output);
+    }
+
+    /**
+     * function that responses with an array key=>'output string'
+     *
+     * @param array $key_path array of the keys of the current values
+     * @param mixed $expected
+     * @param mixed $actual
+     * @param int $value_expectation could be (VALUE_NOT_EXPECTED_TO_BE, VALUE_EXPECTED_TO_BE_DIFFERENT)
+     * @return array
+     */
+    function makeResponse($key_path, $expected, $actual, $expectation = self::VALUE_NOT_EXPECTED_TO_BE, $separator = self::DEFAULT_SEPARATOR)
+    {
+       
+        $currentKey = array_pop($key_path);
+        if ($expectation == self::VALUE_NOT_EXPECTED_TO_BE) {
+            $expectation = sprintf("%s exists, but expected not to be", $expected);
+        } elseif ($expectation == self::VALUE_EXPECTED_TO_BE) { 
+            $expectation = sprintf("%s does not exists, but expected to be", $expected);
+        } elseif ($expectation == self::VALUE_EXPECTED_TO_BE_DIFFERENT) {
+            $expectation =  sprintf("is \"%s\", but expected to be \"%s\"", $expected, $actual);
+        }
+        $key = sprintf("%s %s", implode($separator, $key_path), $currentKey);
+        return array($key => $expectation);
+    }
+
+    /**
+     * function that handles the differences between two array
+     *
+     * @param mixed $expected
+     * @param mixed $actual
+     * @param int $value_expectation could be (VALUE_NOT_EXPECTED_TO_BE, VALUE_EXPECTED_TO_BE_DIFFERENT)
+     * @param array $key_path array of the keys of the current values
+     * @return array an associative array of difference
+     */
+    function doComparison($expected, $actual, $value_expectation = 1, $key_path = array())
+    {
+        
+        if ($expected == $actual) {
+            return array(); 
+        } elseif (is_array($expected) && is_array($actual)) {
+            $brothers = array();
+            foreach ($expected as $key => $value) {
+                array_push($key_path, $key);
+                if (isset($actual[$key])) {
+                    $comparison = $this->doComparison($value, $actual[$key], $value_expectation, $key_path);
+                    $brothers = array_merge($brothers, $comparison);
+                } else {
+                    // if not exists on b
+                    $brothers = array_merge($brothers, $this->makeResponse($key_path, $expected, $actual, $value_expectation));
+                }
+                array_pop($key_path);
+            }
+            return $brothers;
+        } else {
+            return $this->makeResponse($key_path, $expected, $actual, self::VALUE_EXPECTED_TO_BE_DIFFERENT);
+        }
     }
 }
